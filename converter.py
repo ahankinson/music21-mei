0a2a966d14542ea5274cf99b10e2104766e2a3c2
Andrew Hankinson <andrew.hankinson@gmail.com>
2010-12-03 11:03:08 -0500

    rewriting converter. Again.

--- converter.py
+++ converter.py
@@ -36,7 +36,6 @@ class ConverterMei(object):
         self._meiDoc = None
         self._score = stream.Score()
         self._sections = {}
-        self._score_defs = []
         
         self._currentTimeSig = None # time signatures can change. This is the current one.
         self._timeSigHasChanged = False # set this to true if we hit a tsig change
@@ -48,118 +47,72 @@ class ConverterMei(object):
         self._currentClef = None
         self._clefHasChanged = False
         
-        self._staves = {} # {staffnum: <m21 staff object>}
+        self._parts = {} # {sid-nid: <m21 Part>}
         
         self.__flat_score = None # the flattened objects in the score.
         
     
     def parseFile(self, fn):
         self._meiDoc = convert(fn)
-        self._firstPass()
-        self._secondPass()
+        self._createRegistries()
         
         #self._score.append(self._staves.values())
         
         #self._score.show('text')
         pass
     
-    def _firstPass(self):
-        """ 
-            Work out what the sections and global values are we need, and 
-            set us up the score.
-        """
+    def _createRegistries(self):
+        # creates a couple registries for lookups.
+        # object-registry:
+        #  { element.id: <Music21 Object> }
+        #  where element.id is the XML/MEI xml:id, and the corresponding Music21 object.
+        #
+        # staff-part registry:
+        #  {secnum-sid-lid: <Music21 Part> }
+        # where secid-sid-lid is the section number, staff number and layer number used
+        # as a lookup for a part. If that does not exist, a new part is created.
         
-        self.__flat_score = flatten(self._meiDoc.search('score')[0])
-        score_elements = (
-            'scoredef',
-            'staffdef',
-            'layerdef',
-            'instrdef',
-        )
-        self._score_defs = filter(lambda s: s.name in score_elements, self.__flat_score)
-        lg.debug("Score Definitions: ".format(pprint.pprint(self._score_defs)))
+        #==== object registry
         
-        # set some defaults based on the scoredef header section
-        sc = filter(lambda s: s.getname() == 'scoredef', self._score_defs)[0]
         
-        mc = sc.attribute_by_name('meter.count').value
-        mu = sc.attribute_by_name('meter.unit').value
-        self._currentTimeSig = meter.TimeSignature()
-        self._currentTimeSig.load("{0}/{1}".format(mc, mu))
-        
-        ks = self._keysigConverter(sc.attribute_by_name('key.sig').value)
-        self._currentKeySig = key.KeySignature(ks)
-        
-        sections = filter(lambda s: s.name == "section", self.__flat_score)
-        section_elements = (
-            'scoredef',
-            'measure',
-            'staff',
-            'layer',
-            'note',
-            'rest',
-            'mrest',
-            'beam',
-            'dynam',
-            'slur',
-            'hairpin',
-            'fermata',
-            'mordent'
-        )
-        
-        # we don't need to encode sections, but having them sorted here makes it pretty
-        # handy to work with them.
-        for section in sections:
-            se = filter(lambda s: s.name in section_elements, flatten(section))
-            self._sections[section] = se
-        lg.debug("Section elements: {0}".format(pprint.pprint(self._sections)))
-        
-        # 
-        # lg.debug("Staves: {0}".format(self._staves))
-        
-    def _secondPass(self):
-        # this is where it goes all squirrely. In MEI, measures are parents of voices
-        # in Music21, parts are parents of measures. This simple fact keeps me up at night.
-        
-        # 1. Get staff definitions, if they exist.
-        
-        # 2. Something.
-        
-        # 3. Once we have all the staves identified, find out, for each one, how many
-        #  voices ("layers") they have.
-        
-        # 4. If the staff already has a part defined with the same ID, that's OK; otherwise
-        #  add a new voice ("part") to the staff.
-        
-        # 5. Now we can parse the sections. Get the section stuff that we parsed earlier
-        #  and iterate through them.
-        
-        # 6. In this section, see if we have a scoredef as the first element. If so, trigger some
-        #  of the state changes if we do so we can apply that to the measure creation.
-        
-        # 7. For all the layers in this section, grab the previously defined part on this staff
-        #  in this MEI measure.
-        
-        # 8. Add the notes to the measure.
-        
-        # 9. Add the measure to the part
-        
-        # 10. Add the part to the staff (?)
-        
-        # 11. Add the staff to the staff group
-        
-        # 12. Add the staff group to the score.
-        pass
-    
+        #==== staff-part registry =====
+        sections = self._meiDoc.search('section')
+        for secnum, section in enumerate(sections):
+            staves = section.descendents_by_name('staff')
+            self._sections[secnum] = {}
+            
+            for staff in staves:
+                staffnum = staff.attribute_by_name('n').value
+                layers = staff.descendents_by_name('layer')
+                
+                for layer in layers:
+                    laynum = layer.attribute_by_name('n').value
+                    pid = "{0}-{1}".format(staffnum, laynum)
+                    
+                    # sometimes we may have more than one definition of 
+                    # a part in a section. This occurs if there are different
+                    # readings. For now, we'll be naive and assume that we won't be
+                    # parsing that type of riffraff, but we'll check our backs anyway,
+                    # lest we get a key collision that could change the entire course of humanity.
+                    if pid not in self._sections[secnum].keys():
+                        self._sections[secnum][pid] = stream.Part()
+                        
     def _thirdPass(self):
         # This goes through and looks for "spanner" elements like slurs, ties, triplets,
         # etc. When it finds one, it adds it to the appropriate element.
         pass
     
     
-    def _calculateParts(self, section):
-        # we need to find out how many parts are in this section.
-        
+    
+    
+    def _calculatePart(self, obj):
+        # given an object (note, rest, etc.), calculates the part lookup for the staff-part
+        # registry lookup.
+        # this helps determine which part this object belongs to.
+        sn, ln = None
+        sn = obj.ancestor_by_name('staff').attribute_by_name('n').value
+        ln = obj.ancestor_by_name('layer').attribute_by_name('n').value
+        pid = "s{0}-l{1}".format(sn, ln)
     
     def _createMeasure(self, measure_element):
         m_measure = stream.Measure()
